4ad4275191d4abbaa7f16fad91c4a9768b879ffe
Peter Ugh-GitHub <peterp.lyle@gmail.com>
2022-02-20 21:20:51 -0600

    part 1 completed

---
+++ src/main/java/com/ughgithub/payroll/PayrollApplication.java
@@ -0,0 +1,18 @@
+// package com.ughgithub.payroll;
+
+// import org.springframework.boot.SpringApplication;
+
+// import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// @SpringBootApplication
+// public class PayrollApplication {
+
+// 	public static void main(String[] args) {
+// 		try {
+// 			SpringApplication.run(PayrollApplication.class, args);			
+// 		} catch (Exception e) {
+// 			e.printStackTrace(); 
+// 		}
+// }
+
+// }
